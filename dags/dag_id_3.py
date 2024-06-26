ebe02cf9aad153dba8b0dd9e942132e9855ed991
RobertPlaiasuGridDynamics <rplaiasu@griddynamics.com>
2024-06-26 11:49:15 +0300

    stage 2

---
+++ dags/dag_id_3.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+
+from resources import *
+
+with DAG(
+        dag_id="dag_id_3",
+        start_date=config["dag_id_3"]["start_date"],
+        schedule=config["dag_id_3"]["schedule_interval"]
+) as dag:
+    task1 = PythonOperator(
+        task_id="log_database_connection",
+        python_callable=log_context,
+        op_kwargs={"dag_id": "dag_id_3", "database": "airflow"},
+        dag=dag
+    )
+    task2 = EmptyOperator(
+        task_id="insert_new_row"
+    )
+    task3 = EmptyOperator(
+        task_id="query_table"
+    )
+    task1 >> task2 >> task3
---
+++ dags/resources.py
@@ -0,0 +1,19 @@
+import logging
+from datetime import datetime
+
+config = {
+    'dag_id_1': {'schedule_interval': None,
+                 "start_date": datetime(2018, 11, 11),
+                 "table_name": "table_name_1"},
+    'dag_id_2': {'schedule_interval': None,
+                 "start_date": datetime(2018, 11, 11),
+                 "table_name": "table_name_2"},
+    'dag_id_3': {'schedule_interval': None,
+                 "start_date": datetime(2018, 11, 11),
+                 "table_name": "table_name_3"}
+}
+
+
+def log_context(ds=None, **kwargs):
+    logger = logging.getLogger(__name__)
+    logger.info(f"{kwargs["dag_id"]} start processing tables in database:{kwargs["database"]}")
