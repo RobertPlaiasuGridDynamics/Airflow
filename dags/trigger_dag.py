c2f9e2ee712bf6915b9e35818170e6879436fac5
RobertPlaiasuGridDynamics <rplaiasu@griddynamics.com>
2024-07-02 16:56:40 +0300

    add variable path

--- dags/trigger_dag.py
+++ dags/trigger_dag.py
@@ -4,15 +4,19 @@ from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.sensors.filesystem import FileSensor
 from resources import *
 from airflow import DAG
+from airflow.models import Variable
+
+path = Variable.get("path")
 
 with DAG(
     dag_id="trigger_dag",
     start_date=datetime(2024,1,1),
     schedule=None
 ) as dag:
+
     task1 = FileSensor(
         task_id="wait_for_run_file",
-        filepath="/Users/rplaiasu/PycharmProjects/DagCreation/data/small_text",
+        filepath=f"{path}",
         dag=dag
     )
     task2 = TriggerDagRunOperator(
@@ -22,7 +26,7 @@ with DAG(
     )
     task3 = BashOperator(
         task_id="remove_small_text.txt",
-        bash_command="rm /Users/rplaiasu/PycharmProjects/DagCreation/data/small_text",
+        bash_command=f"rm {path}",
         dag=dag
     )
 task1 >> task2 >> task3
